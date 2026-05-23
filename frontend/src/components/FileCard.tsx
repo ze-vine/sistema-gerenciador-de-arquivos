c7c93689f5fe926bd2c3de49abd9b7b8deeb4974
José Vine <josevine729@gmail.com>
2026-05-23 18:05:16 -0300

    feat: melhora o alert de exclusão do arquivo

--- frontend/src/components/FileCard.tsx
+++ frontend/src/components/FileCard.tsx
@@ -1,11 +1,12 @@
 import type { IFile } from "../@types/file";
+import { AlertDialogDestructive } from "./AlertDialogDestructive";
 
 interface Props {
   file: IFile;
   onDelete: (id: string) => void;
 }
 
-export function FileCard({ file }: Props) {
+export function FileCard({ file, onDelete }: Props) {
   const isImage = file.type.startsWith("image/");
 
 const openFile = () => {
@@ -59,6 +60,18 @@ const openFile = () => {
             {(file.size / 1024).toFixed(1)} KB
           </span>
         </div>
+      </div>
+      <div className="flex justify-center">
+        <AlertDialogDestructive
+          buttonTitle="EXCLUIR ESTE ARQUIVO"
+          title="Tem certeza de que quer excluir este arquivo?"
+          description="Se você clicar em excluir, a ação não poderá mais ser desfeita."
+          cancelTitle="Cancelar"
+          actionTitle="Excluir"
+          deleteFileFunction={onDelete}
+          deletedFile={file}
+          className="my-3"
+        />
       </div>
     </div>
   );
