fe9ac52bbbf3c1fc9f95934d9f0b160fbcd4d6af
José Vine <josevine729@gmail.com>
2026-06-14 12:47:07 -0300

    feat: adiciona validação de dados para o CreateFolderDto usando a biblioteca class-validator

--- backend/src/folders/dto/create-folder.dto.ts
+++ backend/src/folders/dto/create-folder.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsOptional, IsString, IsUUID } from "class-validator";
+
 export class CreateFolderDto {
+    @IsString({ message: "O campo name precisa ser do tipo string!" })
+    @IsNotEmpty({ message: "O campo name é obrigatório!" })
     name!: string;
-    folderId!: string;
+
+    @IsOptional()
+    @IsUUID(undefined, { message: "Você deve informar um UUID válido para a pasta pai!" })
+    folderId!: string | null;
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,8 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
+
+   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT ?? 3000);
 }
